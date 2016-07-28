9b35eb28908b3b6e06b1ecf0f61565d4165db867
ashukaushik <ashukaushik8395@gmail.com>
2016-07-28 19:30:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashukaushik/fun/MainActivity.java
@@ -0,0 +1,242 @@
+package com.example.ashukaushik.fun;
+
+import android.content.Intent;
+import android.media.MediaMetadataRetriever;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_tabbed, container, false);
+            ArrayList<File> songs;
+            ArrayAdapter<String> adapter;
+            ArrayList<String> songNames;
+            ListView lv=(ListView)rootView.findViewById(R.id.listView) ;
+            songNames=new ArrayList<>();
+            songs=new ArrayList<>();
+            songs=getSongs(Environment.getExternalStorageDirectory());
+            for(int i=0;i<songs.size();i++){
+                MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+                mmr.setDataSource(songs.get(i).getPath());
+                String albumName = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+                if(albumName!=null)
+                    songNames.add(albumName);
+                else
+                    songNames.add(songs.get(i).getName());
+//            songNames.add(songs.get(i).getName().toString());
+            }
+            adapter=new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,songNames);
+            lv.setAdapter(adapter);
+            final ArrayList<File> finalSongs = songs;
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    startActivity(new Intent(getContext(),NowPlaying.class).putExtra("pos",position).putExtra("List", finalSongs));
+                }
+            });
+
+            return rootView;
+        }
+    }
+
+    public static ArrayList<File> getSongs(File root) {
+        ArrayList<File> songs=new ArrayList<>();
+        File[] files=root.getAbsoluteFile().listFiles();
+        for(File singleFile:files){
+            if(singleFile.isDirectory()&& !singleFile.isHidden()){
+                songs.addAll(getSongs(singleFile));
+            }
+            else{
+                if(singleFile.getName().endsWith(".mp3")){
+                    MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+                    mmr.setDataSource(singleFile.getPath());
+                    if(mmr.METADATA_KEY_DURATION<=50000){
+                        songs.add(singleFile);
+                    }
+                }
+            }
+//            Toast.makeText(getApplicationContext(),files.length+"",Toast.LENGTH_LONG).show();
+        }
+        return songs;
+    }
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SONGS";
+                case 1:
+                    return "ALBUMS";
+                case 2:
+                    return "ARTISTS";
+            }
+            return null;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/ashukaushik/fun/NowPlaying.java
@@ -0,0 +1,122 @@
+package com.example.ashukaushik.fun;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class NowPlaying extends AppCompatActivity implements View.OnClickListener {
+    MediaPlayer mp;
+    ArrayList<File> songs;
+    int position;
+    Uri u;
+    SeekBar sb;
+    Button playb;
+    Button prevb;  // changed name of buttons from play to playb and so on. 26-07-2016
+    Button nextb;
+    Button stopb;
+    Thread updateSeekbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
+
+        sb = (SeekBar) findViewById(R.id.seekBar);
+        updateSeekbar = new Thread() {
+            @Override
+            public void run() {
+                int totalDuration = mp.getDuration();
+                int currentPosition = 0;
+
+
+                while (currentPosition < totalDuration) {
+                    try {
+                        sleep(500);
+                        currentPosition = mp.getCurrentPosition();
+                        sb.setProgress(currentPosition);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+        playb = (Button) findViewById(R.id.play);
+        stopb = (Button) findViewById(R.id.stop);
+        prevb = (Button) findViewById(R.id.prev);
+        nextb = (Button) findViewById(R.id.next);
+
+
+        playb.setOnClickListener(this);
+        stopb.setOnClickListener(this);
+        prevb.setOnClickListener(this);
+        nextb.setOnClickListener(this);
+
+        if (mp != null) {
+            mp.stop();
+            mp.release();
+        }
+
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        songs = (ArrayList) b.getParcelableArrayList("List");
+        position = b.getInt("pos", 0);
+        u = Uri.parse(songs.get(position).toString());
+        mp = MediaPlayer.create(getApplicationContext(), u);
+        mp.start();
+        playb.setText("PAUSE");
+        sb.setMax(mp.getDuration());
+        updateSeekbar.start();
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mp.seekTo(seekBar.getProgress());
+            }
+        });
+
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        int id=v.getId();
+        if(id == R.id.play){
+            if(playb.getText()=="PAUSE"){
+                playb.setText("PLAY");
+                mp.pause();
+
+            }else if(playb.getText()=="PLAY"){
+                playb.setText("PAUSE");
+                mp.start();
+            }
+        }else if(id == R.id.stop){
+            playb.setText("PLAY");
+            mp.stop();
+            mp.seekTo(0);
+//            sb.setProgress();
+        }else if(id == R.id.next){
+
+        }else if(id == R.id.prev){
+
+        }
+    }
+
+}
