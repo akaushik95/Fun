98596a81cba73eff5e9ae6ea931ac39abf9854b1
ashukaushik <ashukaushik8395@gmail.com>
2016-08-07 17:55:20 +0530

    THIRD COMMIT NOW PLAYING UI

--- app/src/main/java/com/example/ashukaushik/fun/NowPlaying.java
+++ app/src/main/java/com/example/ashukaushik/fun/NowPlaying.java
@@ -1,46 +1,54 @@
 package com.example.ashukaushik.fun;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
 
 public class NowPlaying extends AppCompatActivity implements View.OnClickListener {
-    MediaPlayer mp;
-    ArrayList<File> songs;
+    MediaPlayer mMediaPlayer;
+    ArrayList<File> songsList;
     int position;
     Uri u;
-    SeekBar sb;
-    Button playb;
-    Button prevb;  // changed name of buttons from play to playb and so on. 26-07-2016
-    Button nextb;
-    Button stopb;
-    Thread updateSeekbar;
+    SeekBar mSeekBar;
+    Button mPlayButton;
+    Button mPreviousSongButton;  // changed name of buttons from play to playb and so on. 26-07-2016
+    Button mNextSongButton;
+    Thread mThreadSeekBar;
+
+    TextView mSongNameTextView;
+    ImageView mAlbumArtImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_playing);
 
-        sb = (SeekBar) findViewById(R.id.seekBar);
-        updateSeekbar = new Thread() {
+        mSongNameTextView=(TextView)findViewById(R.id.songNameTextView);
+        mAlbumArtImageView=(ImageView)findViewById(R.id.albumArtImageView);
+        mSeekBar = (SeekBar) findViewById(R.id.seekBar);
+        mThreadSeekBar = new Thread() {
             @Override
             public void run() {
-                int totalDuration = mp.getDuration();
+                int totalDuration = mMediaPlayer.getDuration();
                 int currentPosition = 0;
-
 
                 while (currentPosition < totalDuration) {
                     try {
                         sleep(500);
-                        currentPosition = mp.getCurrentPosition();
-                        sb.setProgress(currentPosition);
+                        currentPosition = mMediaPlayer.getCurrentPosition();
+                        mSeekBar.setProgress(currentPosition);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -48,33 +56,31 @@ public class NowPlaying extends AppCompatActivity implements View.OnClickListene
             }
         };
 
-        playb = (Button) findViewById(R.id.play);
-        stopb = (Button) findViewById(R.id.stop);
-        prevb = (Button) findViewById(R.id.prev);
-        nextb = (Button) findViewById(R.id.next);
+        mPlayButton=(Button)findViewById(R.id.playButton);
+        mPreviousSongButton=(Button)findViewById(R.id.previousSongButton);
+        mNextSongButton=(Button)findViewById(R.id.nextSongButton);
 
 
-        playb.setOnClickListener(this);
-        stopb.setOnClickListener(this);
-        prevb.setOnClickListener(this);
-        nextb.setOnClickListener(this);
+        mPlayButton.setOnClickListener(this);
+        mPreviousSongButton.setOnClickListener(this);
+        mNextSongButton.setOnClickListener(this);
 
-        if (mp != null) {
-            mp.stop();
-            mp.release();
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
         }
 
         Intent i = getIntent();
         Bundle b = i.getExtras();
-        songs = (ArrayList) b.getParcelableArrayList("List");
+        songsList = (ArrayList) b.getParcelableArrayList("List");
         position = b.getInt("pos", 0);
-        u = Uri.parse(songs.get(position).toString());
-        mp = MediaPlayer.create(getApplicationContext(), u);
-        mp.start();
-        playb.setText("PAUSE");
-        sb.setMax(mp.getDuration());
-        updateSeekbar.start();
-        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        u = Uri.parse(songsList.get(position).toString());
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), u);
+        mMediaPlayer.start();
+//        setScreen(position,songsList);
+        mSeekBar.setMax(mMediaPlayer.getDuration());
+        mThreadSeekBar.start();
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
@@ -87,7 +93,7 @@ public class NowPlaying extends AppCompatActivity implements View.OnClickListene
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                mp.seekTo(seekBar.getProgress());
+                mMediaPlayer.seekTo(seekBar.getProgress());
             }
         });
 
@@ -98,26 +104,38 @@ public class NowPlaying extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         int id=v.getId();
-        if(id == R.id.play){
-            if(playb.getText()=="PAUSE"){
-                playb.setText("PLAY");
-                mp.pause();
-
-            }else if(playb.getText()=="PLAY"){
-                playb.setText("PAUSE");
-                mp.start();
+        if(id == R.id.playButton){
+            if(mMediaPlayer.isPlaying()){
+                mPlayButton.setBackground(getDrawable(R.drawable.ic_play_circle_filled_white_black_48dp));
+                mMediaPlayer.pause();
+            }else{
+                mMediaPlayer.start();
+                mPlayButton.setBackground(getDrawable(R.drawable.ic_pause_circle_filled_black_48dp));
             }
-        }else if(id == R.id.stop){
-            playb.setText("PLAY");
-            mp.stop();
-            mp.seekTo(0);
-
-//            sb.setProgress();
-        }else if(id == R.id.next){
-
-        }else if(id == R.id.prev){
-
+        }else if(id == R.id.nextSongButton){
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            position=(position+1)%songsList.size();
+            u=Uri.parse(songsList.get(position).toString());
+            mMediaPlayer=MediaPlayer.create(getApplicationContext(),u);
+            mMediaPlayer.start();
+            mSeekBar.setMax(mMediaPlayer.getDuration());
+            mPlayButton.setBackground(getDrawable(R.drawable.ic_pause_circle_filled_black_48dp));
+        }else if(id == R.id.previousSongButton){
+            mMediaPlayer.stop();
+            mMediaPlayer.release();
+            position=(position-1)<0?songsList.size()-1:position-1;
+            u=Uri.parse(songsList.get(position).toString());
+            mMediaPlayer=MediaPlayer.create(getApplicationContext(),u);
+            mMediaPlayer.start();
+            mSeekBar.setMax(mMediaPlayer.getDuration());
+            mPlayButton.setBackground(getDrawable(R.drawable.ic_pause_circle_filled_black_48dp));
         }
     }
 
+//    public void setScreen(int position, ArrayList<File> songsList){
+//        MediaMetadataRetriever mMediaMetadataRetriever=new MediaMetadataRetriever();
+//        mMediaMetadataRetriever.setDataSource(songsList.get(position).getPath());
+//        mSongNameTextView.setText(mMediaMetadataRetriever.METADATA_KEY_TITLE);
+//    }
 }
